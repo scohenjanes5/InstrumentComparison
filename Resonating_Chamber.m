91240f1bcf95b7d80369fe13e2619efdb8556814
scohenjanes5 <scohenjanes@brandeis.edu>
2023-03-21 22:03:03 -0400

    Average envelope works

--- Resonating_Chamber.m
+++ Resonating_Chamber.m
@@ -1,8 +1,19 @@
 % Set the path to the folder containing the audio files
 folder_path="C:\Users\sande\Desktop\school\Senior Spring\Math and Music\InstrumentComparison\Recordings\Jupiter\Notes\";
 %%
+plotEnvs(folder_path, 25000);
+%%
+[envMatrix, avgEnv] = plotEnvs2(folder_path, 25000);
 
-plotEnvs(folder_path,25000);
+%%
+[f_trim, envelope_trim] = get_Env("C:\Users\sande\Desktop\school\Senior Spring\Math and Music\InstrumentComparison\Recordings\Jupiter\Notes\C5.wav", 2500);
+[f_trim2, envelope_trim2] = get_Env("C:\Users\sande\Desktop\school\Senior Spring\Math and Music\InstrumentComparison\Recordings\Jupiter\Notes\C4.wav", 2500);
+
+envMatrix = [envelope_trim; envelope_trim2];
+
+avgEnv = mean(envMatrix,1);
+
+disp(avgEnv)
 
 %% Get envelope from file
 function [envelope] = GetEnvelope(filename)
@@ -57,7 +68,8 @@ function plotEnvs(folder_path, f_cutoff)
         envelope_trim = envelope(1:idx_cutoff);
         
         % Plot the envelope
-        plot(f(1:idx_cutoff), envelope_trim, 'DisplayName', audio_files(i).name);
+        f=f(1:idx_cutoff);
+        plot(f, envelope_trim, 'DisplayName', audio_files(i).name);
     end
     
     % Set plot title and axis labels
@@ -66,3 +78,62 @@ function plotEnvs(folder_path, f_cutoff)
     ylabel('Magnitude (dB)');
     legend('show', 'Location', 'northeast');
 end
+
+
+
+%% Get all envselopes in a folder
+function [f_trim, envelope_trim] = get_Env(file_path, f_cutoff)
+    [~, fs] = audioread(file_path);
+        
+    % Get the envelope of the audio file
+    envelope = GetEnvelope(file_path);
+    
+    % Trim the envelope
+    f = (0:length(envelope)-1)*(fs/length(envelope));
+    [~, idx_cutoff] = min(abs(f - f_cutoff));
+    
+    f_trim=f(1:idx_cutoff);
+    envelope_trim = envelope(1:idx_cutoff);
+end
+
+%%
+function [envMatrix, avgEnv] = plotEnvs2(folder_path, f_cutoff)
+    close all
+    % Get a list of all audio files in the folder
+    audio_files = dir(fullfile(folder_path, '*.wav'));
+    
+    % Initialize envelope matrix
+    envMatrix = [];
+
+    % Loop through all audio files and plot their envelopes
+    figure;
+    hold on;
+    for i = 1:length(audio_files)
+        % Load the audio file
+        file_path = fullfile(folder_path, audio_files(i).name);
+        [f_trim, envelope_trim] = get_Env(file_path, f_cutoff);
+       
+        % Add envelope to envelope matrix
+        envMatrix = [envMatrix; envelope_trim];
+        plot(f_trim, envelope_trim, 'DisplayName', audio_files(i).name);
+    end
+    
+    % Compute average envelope
+    avgEnv = mean(envMatrix, 1);
+    
+
+    % Plot individual envelopes
+    title('Spectral Envelopes of Audio Files');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
+    legend('show', 'Location', 'northeast');
+    
+    % Plot average envelope
+    figure;
+    plot(f_trim, avgEnv, 'DisplayName', 'Average Envelope');
+    title('Average Spectral Envelope of Audio Files');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
+    legend('show', 'Location', 'northeast');
+end
+
